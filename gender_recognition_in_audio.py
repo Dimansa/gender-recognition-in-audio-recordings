bb8be05b2fcbc53f686d607aa593274a2bde5222
Dimansa <97978484+Dimansa@users.noreply.github.com>
2022-09-01 19:23:09 +0300

    Update gender_recognition_in_audio.py

--- gender_recognition_in_audio.py
+++ gender_recognition_in_audio.py
@@ -1,55 +1,29 @@
-import sys
-import numpy as np
-import aubio
-from aubio import source, pitch
-import amfm_decompy.pYAAPT as pYAAPT
+importimport amfm_decompy.pYAAPT as pYAAPT
 import amfm_decompy.basic_tools as basic
-import matplotlib.pyplot as plt
+pitchY = [0]
+s = 0
+print('введите количество аудиозаписей')
+n = int(input())
+for i in range(1, n+1):
+    signal = basic.SignalObj('{id}.wav'.format(id=i))
+    filename = '{id}.wav'.format(id=i)
+    print(filename, end=' ')
+    pitchY.append(pYAAPT.yaapt(signal, frame_length=35, tda_frame_length=35, f0_max=600))
+    minS = 2000
+    maxS = 0
+    sumS = 0
+    j = 0
+    for t in range(len(pitchY[i].samp_values)):
+        if float(pitchY[i].samp_values[t]) > 0:
+            sumS += float(pitchY[i].samp_values[t])
+            j += 1
+            if minS > float(pitchY[i].samp_values[t]):
+                minS = float(pitchY[i].samp_values[t])
+            if maxS < float(pitchY[i].samp_values[t]):
+                 maxS = float(pitchY[i].samp_values[t])
+    if sumS/j > 155:
+        print('женщина')
+    else:
+        print('мужчина')
 
-# load audio
-signal = basic.SignalObj('test_vad_1.wav')
-filename = 'test_vad_1.wav'
-
-# YAAPT pitches
-pitchY = pYAAPT.yaapt(signal, frame_length=40, tda_frame_length=40, f0_min=75, f0_max=600)
-
-# YIN pitches
-downsample = 1
-samplerate = 0
-win_s = 1764 // downsample  # fft size
-hop_s = 441 // downsample  # hop size
-s = source(filename, samplerate, hop_s)
-samplerate = s.samplerate
-tolerance = 0.8
-pitch_o = pitch("yin", win_s, hop_s, samplerate)
-pitch_o.set_unit("midi")
-pitch_o.set_tolerance(tolerance)
-
-pitchesYIN = []
-confidences = []
-
-total_frames = 0
-while True:
-    samples, read = s()
-    pitch = pitch_o(samples)[0]
-    pitch = int(round(pitch))
-    confidence = pitch_o.get_confidence()
-    pitchesYIN += [pitch]
-    confidences += [confidence]
-    total_frames += read
-    if read < hop_s: break
-
-# load PRAAT pitches
-praat = np.genfromtxt('123.txt', filling_values=0)
-praat = praat[:, 1]
-
-# plot       
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, sharey=True, figsize=(12, 8))
-ax1.plot(np.asarray(pitchesYIN), label='YIN', color='green')
-ax1.legend(loc="upper right")
-ax2.plot(pitchY.samp_values, label='YAAPT', color='blue')
-ax2.legend(loc="upper right")
-ax3.plot(praat, label='Praat', color='red')
-ax3.legend(loc="upper right")
-plt.show()
 
